3934eed474f6fcd8874797ed5bf3afb56c0b1aa2
AsheeshKumar34 <asheesh.kumar3496@gmail.com>
2020-11-10 22:56:56 +0530

    Icon improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
   return (
@@ -29,11 +31,6 @@ function Navbar() {
               Become a host
             </a>
           </li>
-          <li className="nav-item active">
-            <a className="nav-link" href="/">
-              Become a host
-            </a>
-          </li>
           <li className="nav-item dropdown">
             <a
               className="nav-link dropdown-toggle"
@@ -44,19 +41,21 @@ function Navbar() {
               aria-haspopup="true"
               aria-expanded="false"
             >
+              <FontAwesomeIcon icon={faGlobe} />
             </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
               <a className="dropdown-item" href="/">
-                Action
+                English (IN)
               </a>
               <a className="dropdown-item" href="/">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="/">
-                Something else here
+                INR
               </a>
             </div>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link user pt-0" href="/">
+              <FontAwesomeIcon icon={faUserCircle} />
+            </a>
           </li>
         </ul>
       </div>
